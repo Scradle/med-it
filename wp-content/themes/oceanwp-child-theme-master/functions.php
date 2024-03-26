74b8d8de26665651bf33fc39dae791e13b45ceac
Scradle <152749477+Scradle@users.noreply.github.com>
2024-03-26 01:10:44 +0100

    autre approche pour la correction de la position du bouton nous contacter

--- wp-content/themes/oceanwp-child-theme-master/functions.php
+++ wp-content/themes/oceanwp-child-theme-master/functions.php
@@ -33,7 +33,7 @@ add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 
 
 function contact_btn_menu( $items, $args ) {
-	$items .= '<a href="/contact" class="contact-btn">Nous contacter</a>';
+	$items .= '<li class="menu-item-contact"><a class="contact-btn" href="/contact" >Nous contacter</a></li>';
 	return $items;
 }
 
